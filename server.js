a2fa5f02dd18213a87c8ad0432d44cfd55dbfdcc
(nguyendatvinhphat) <(phatnguyen.111020@gmail.com)>
2026-05-21 22:48:15 +0700

    Add CORS headers to server

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 app.use(express.json({ limit: '10mb' }));
+app.use((req, res, next) => {
+    res.set('Access-Control-Allow-Origin', '*');
+    res.set('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    if (req.method === 'OPTIONS') return res.status(200).end();
+    next();
+});
 
 const DATA_DIR = '/tmp';
 const PORT = process.env.PORT || 3000;
